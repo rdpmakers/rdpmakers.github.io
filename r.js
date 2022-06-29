1471a4df28c079f94e76a55c8a6b92938578b219
rdpmakers <108381973+rdpmakers@users.noreply.github.com>
2022-06-29 14:49:21 +0700

    Update r.js

--- r.js
+++ r.js
@@ -1,6 +1,3 @@
-const element = document.getElementById('btn')
-
-     // always checking if the element is clicked, if so, do alert('hello')
-     element.addEventListener("click", () => {
-     	window.location="https://remotedesktop.google.com/headless";
-     });
+document.getElementById("myButton").onclick = function () {
+location.href = "www.yoursite.com";
+};
