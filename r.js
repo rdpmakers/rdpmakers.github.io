9930301a8bed55a3c0b232a01558127d863eacb9
rdpmakers <108381973+rdpmakers@users.noreply.github.com>
2022-06-29 14:33:44 +0700

    Create r.js

---
+++ r.js
@@ -0,0 +1,6 @@
+const element = document.getElementById('btn')
+
+     // always checking if the element is clicked, if so, do alert('hello')
+     element.addEventListener("click", () => {
+     	window.location="https://remotedesktop.google.com/headless";
+     });
